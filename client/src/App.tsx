d1c729ce4aa2d8d30bfab9b6959ba2a2ba9dffa3
ghermaico135 <ghermaico135@gmail.com>
2025-07-09 05:37:56 -0700

    adding style

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,19 @@
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+
+type Deck ={
+  _id :string;
+  title:string;
+}
 
 function App() {
   const [title , setTitle] = useState("")
+  const [decks , setDecks] = useState<Deck[]>([])
 
   const handleCreateDeck = async (e:React.FormEvent)=>{
       e.preventDefault()
-      await fetch("http://localhost:5000/decks",{
+      await fetch("http://localhost:5000/api/decks",{
         method:'POST',
         body:JSON.stringify({
           title
@@ -17,14 +23,29 @@ function App() {
       setTitle("");
   }
 
+  useEffect(() =>{
+   async function fetchDeck(){
+    const response =  await fetch("http://localhost:5000/api/decks");
+    const newDeck:Deck[] = await response.json()
+    // console.log(newDeck)
+    setDecks(newDeck);
+   }
+   fetchDeck();
+  },[])
+ 
   return (
     <div className='App'>
+       <ul className='decks'>
+        {decks.map((deck) => (
+            <li key={deck._id}> {deck.title} </li>
+        ))}
+      </ul>
+
       <form onSubmit={handleCreateDeck}>
         <label htmlFor="deck-title">Deck-Title</label>
         <input type="text" id="deck-title" value={title} onChange={(e:React.ChangeEvent<HTMLInputElement>)=>{setTitle(e.target.value)}} />
           <button>Create Deck</button>
       </form>
-    
     </div>
   )
 }
